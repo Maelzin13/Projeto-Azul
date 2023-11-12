3ccdb69b5636b5cb8d7e00b65ba750e0cfd05e01
Ismael dos Santos Dias <mael10774@gmail.com>
2023-11-12 00:12:29 -0300

    Background

--- src/pages/forms/resetComarch.js
+++ src/pages/forms/resetComarch.js
@@ -4,27 +4,41 @@ import { Grid, Button } from '@mui/material';
 import Layout from 'layout';
 import Page from 'components/Page';
 import { useRouter } from 'next/router';
+import { styled } from '@mui/system';
+
+const Background = styled('div')({
+  backgroundImage: "url('/assets/images/maintenance/img1Borrada.png')",
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
+  minHeight: '100vh', // Cobrir toda a altura da tela
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center'
+});
 
 const ComarchReset = () => {
   const router = useRouter();
 
   return (
-    <Page title="Forms Wizard">
-      <Grid container spacing={2.5} justifyContent="center">
-        <Grid item xs={12} md={6} lg={7} container justifyContent="center" spacing={2}>
-          <Grid item>
-            <Button variant="contained" color="secondary" onClick={() => router.replace('/forms/comarch')}>
-              Reset
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button variant="contained" color="primary" onClick={() => router.replace('/forms/comarch')}>
-              Desbloqueio
-            </Button>
+    <Background>
+      <Page title="Forms Wizard">
+        <Grid container spacing={2.5} justifyContent="center">
+          <Grid item xs={12} md={6} lg={7} container justifyContent="center" spacing={2}>
+            <Grid item>
+              <Button variant="contained" color="secondary" onClick={() => router.replace('/forms/comarch')}>
+                Reset
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" color="primary" onClick={() => router.replace('/forms/comarch')}>
+                Desbloqueio
+              </Button>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </Page>
+      </Page>
+    </Background>
   );
 };
 
