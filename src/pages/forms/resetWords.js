8fccb632b05e96db9e2980a05f461d7ac0eeed50
Felcagod <t3ddygamer105@gmail.com>
2023-11-12 22:55:35 -0300

    Feito a personalizacao dos botoes de reset e desbloqueio do WORDTRACER (Fique a vontade para mudar a cor se quiser :)

--- src/pages/forms/resetWords.js
+++ src/pages/forms/resetWords.js
@@ -5,29 +5,57 @@ import Layout from 'layout';
 import Page from 'components/Page';
 import { useRouter } from 'next/router';
 
+
 const WordTracerReset = () => {
   const router = useRouter();
+  
 
   return (
-    <Page title="Forms Wizard">
-      <Grid container spacing={2.5} justifyContent="center">
-        <Grid item xs={12} md={6} lg={7} container justifyContent="center" spacing={2}>
-          <Grid item>
-            <Button variant="contained" color="secondary" onClick={() => router.replace('/forms/words')}>
-              Reset
-            </Button>
-          </Grid>
-          <Grid item>
-            <Button variant="contained" color="primary" onClick={() => router.replace('/forms/words')}>
-              Desbloqueio
-            </Button>
-          </Grid>
-        </Grid>
+    <Grid container justifyContent="center" alignItems="center" style={{ minHeight: '100vh' }}>
+    <Grid item xs={12} md={6} lg={7} container justifyContent="center" spacing={2} style={{ marginBottom: '1200px' }}>
+      {/* Ajuste a propriedade marginBottom conforme necessário */}
+      <Grid item>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={() => router.replace('/forms/words')}
+          style={{
+            textTransform: 'none',
+            borderRadius: 8,
+            padding: '10px 20px',
+            backgroundColor: '#F44336',
+            color: '#FFFFFF',
+            '&:hover': {
+              backgroundColor: '#D32F2F',
+            },
+          }}
+        >
+          Resetar
+        </Button>
       </Grid>
-    </Page>
-  );
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => router.replace('/forms/words')}
+          style={{
+            textTransform: 'none',
+            borderRadius: 8,
+            padding: '10px 20px',
+            backgroundColor: '#2196F3',
+            color: '#FFFFFF',
+            '&:hover': {
+              backgroundColor: '#1565C0',
+            },
+          }}
+        >
+          Desbloquear
+        </Button>
+      </Grid>
+    </Grid>
+  </Grid>
+);
 };
-
 WordTracerReset.getLayout = function getLayout(page) {
   return <Layout>{page}</Layout>;
 };
